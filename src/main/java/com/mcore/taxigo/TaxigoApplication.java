680d77701e51f59774be2349dd83df48e5eebae5
Carlos Moreno <morenort1402@gmail.com>
2026-02-04 18:25:40 +0100

    init

---
+++ src/main/java/com/mcore/taxigo/TaxigoApplication.java
@@ -0,0 +1,13 @@
+package com.mcore.taxigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcore/taxigo/TaxigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcore.taxigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
